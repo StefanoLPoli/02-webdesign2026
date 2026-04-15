d93277c0179fbd5e38c052c2b5349b7c04752e5e
Stefano Lorini <stefano.lorini@mail.polimi.it>
2026-04-15 11:50:04 +0200

    tokens css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,25 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
 	import "$lib/styles/normalize.css";
+	import "$lib/styles/tokens.css";
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	<!-- Satoshi font -->
+	<link rel="preconnect" href="https://api.fontshare.com/">
+	<link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,2&display=swap" rel="stylesheet">
+
+	<!-- Inter font -->
+	<link rel="preconnect" href="https://fonts.bunny.net/">
+	<link rel="preconnect" href="https://fonts.bunny.net/" crossorigin>
+	<link href="https://fonts.bunny.net/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+
+	<link rel="preconnect" href="https://rsms.me/">
+	<link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 </svelte:head>
 
 {@render children()}
